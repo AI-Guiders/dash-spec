69d74b50322f6b17beae751ba83828b8a6828da8
Каратаев Дмитрий <dkarataev1990@gmail.com>
2026-09-08 01:27:30 +0300

    WIP snapshot (uncommitted work found in repo; preserved by Ток, forum 006)

--- src/DashSpec.Host/Commands/ShowCommandPaths.cs
+++ src/DashSpec.Host/Commands/ShowCommandPaths.cs
@@ -1,26 +1,52 @@
-#nullable enable
-
-namespace DashSpec.Host.Commands;
-
-internal static class ShowCommandPaths
-{
-    public const string RootVerb = "show";
-    public const string HostBranch = "host";
-
-    public static string SurfacePath(string surfaceId) =>
-        DashboardCatalogPhrases.Materialize(
-            DashboardCatalogPhrases.ShowHostPhrase,
-            new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
-            {
-                ["surface"] = surfaceId,
-            });
-
-    public static string? ReadSurfaceId(string canonicalPath) =>
-        DashboardCatalogPhrases.TryReadSlot(
-            DashboardCatalogPhrases.ShowHostPhrase,
-            canonicalPath,
-            "surface",
-            out var surfaceId)
-            ? surfaceId
-            : null;
-}
+#nullable enable
+
+
+
+namespace DashSpec.Host.Commands;
+
+
+
+internal static class ShowCommandPaths
+
+{
+
+    public const string RootVerb = "show";
+
+    public const string HostBranch = "host";
+
+
+
+    public static string SurfacePath(string surfaceId) =>
+
+        DashboardCatalogPhrases.Materialize(
+
+            DashboardCatalogPhrases.ShowHostPhrase,
+
+            new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+
+            {
+
+                ["surface"] = surfaceId,
+
+            });
+
+
+
+    public static string? ReadSurfaceId(string canonicalPath) =>
+
+        DashboardCatalogPhrases.TryReadSlot(
+
+            DashboardCatalogPhrases.ShowHostPhrase,
+
+            canonicalPath,
+
+            "surface",
+
+            out var surfaceId)
+
+            ? surfaceId
+
+            : null;
+
+}
+
